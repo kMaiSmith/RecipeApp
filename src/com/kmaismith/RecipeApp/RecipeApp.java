324b8da49d2249d2bd1437eb21e4e953dfd9f057
Kyle Smith <kmaismith@gmail.com>
2013-09-09 10:36:01 -0400

    Initial android app generated

---
+++ src/com/kmaismith/RecipeApp/RecipeApp.java
@@ -0,0 +1,15 @@
+package com.kmaismith.RecipeApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RecipeApp extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
