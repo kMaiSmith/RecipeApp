1392d3561bd6addc5de55e71c4a8f4fcecc0bfe9
shieldscr <cshields@pillartechnology.com>
2013-09-10 12:54:13 -0400

    Story: As A User I Want to Add a New Ingredient

--- src/com/kmaismith/RecipeApp/RecipeApp.java
+++ src/com/kmaismith/RecipeApp/RecipeApp.java
@@ -1,15 +1,41 @@
 package com.kmaismith.RecipeApp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class RecipeApp extends Activity
 {
+    ArrayList<String> RecipeList = new ArrayList<String>();
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    public void submitRecipe(View view) {
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+
+        String inputRecipe = editText.getText().toString();
+
+        RecipeList.add(inputRecipe);
+        editText.setText("");
+
+        Context context = getApplicationContext();
+        CharSequence text = inputRecipe + " Added to Ingredient List";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.setGravity(Gravity.CENTER|Gravity.CENTER, 0, 0);
+        toast.show();
     }
 }
